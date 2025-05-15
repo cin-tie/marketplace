bd7c8a8e296ff921b264326044df5b6fa00c6c9f
cin.tie <csizegod@gmail.com>
2025-05-15 10:08:39 +0300

    Delete logging

--- marketplace-backend/src/main/java/com/cintie/marketplace_backend/controllers/TelegramController.java
+++ marketplace-backend/src/main/java/com/cintie/marketplace_backend/controllers/TelegramController.java
@@ -1,7 +1,5 @@
 package com.cintie.marketplace_backend.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,8 +28,6 @@ public class TelegramController {
     private EmailService emailService;
     private TokenUtils tokenUtils;
     private UserService userService;
-
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/verify")
     public ResponseEntity<?> verifyTelegram(@RequestBody TelegramVerifyRequest request) {
